417213f3c0329394bf6348203c905a58281e038b
刘向东 <liuxiangdong@yidian-inc.com>
2019-12-07 23:30:39 +0800

    update: comment.

--- library/src/main/java/com/liuxiangdong/jsonview/vm/JsonArrayBeginViewModel.java
+++ library/src/main/java/com/liuxiangdong/jsonview/vm/JsonArrayBeginViewModel.java
@@ -18,7 +18,7 @@ package com.liuxiangdong.jsonview.vm;
 import com.liuxiangdong.jsonview.entry.JsonArrayEntry;
 
 /**
- * A opening JSONArray item.
+ * An opening JSONArray item.
  */
 public class JsonArrayBeginViewModel extends JsonCompoundViewModel<JsonArrayEntry> {
     public JsonArrayBeginViewModel(String key, JsonArrayEntry compoundEntry, int depth, int parentEntryCount, int index) {
