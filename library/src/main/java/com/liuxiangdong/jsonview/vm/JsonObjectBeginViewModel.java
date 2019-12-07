82955b2d38a03e936d5e2c682ca2585a464afa13
刘向东 <liuxiangdong@yidian-inc.com>
2019-12-07 23:33:23 +0800

    update: comment.

--- library/src/main/java/com/liuxiangdong/jsonview/vm/JsonObjectBeginViewModel.java
+++ library/src/main/java/com/liuxiangdong/jsonview/vm/JsonObjectBeginViewModel.java
@@ -18,7 +18,7 @@ package com.liuxiangdong.jsonview.vm;
 import com.liuxiangdong.jsonview.entry.JsonObjectEntry;
 
 /**
- * A opening JSONObject item.
+ * An opening JSONObject item.
  */
 public class JsonObjectBeginViewModel extends JsonCompoundViewModel<JsonObjectEntry> {
     public JsonObjectBeginViewModel(String key, JsonObjectEntry compoundEntry, int depth, int parentEntryCount, int index) {
